e33ada515e0b7a6f840e617d12e1b7cc38e89b67
adrianrgd <adrirandor@gmail.com>
2026-01-12 14:48:14 +0100

    12/01/2026

--- PROGRAMACION/Unidad 3 - Estructuras de Almacenamiento/U3.1 Arrays/JuegoCiudades.java
+++ PROGRAMACION/Unidad 3 - Estructuras de Almacenamiento/U3.1 Arrays/JuegoCiudades.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Arrays; // Agregado para usar Arrays.toString()
 
 public class JuegoCiudades {
     public static void main(String[] args) {
